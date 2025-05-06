54a8bb92ad323e0acf14401a2e59f593fbd901e6
Alberto García <malgardel@gmail.com>
2025-05-06 23:45:31 +0200

    Check livewire

---
+++ html/resources/views/livewire/counter.blade.php
@@ -0,0 +1,35 @@
+<div class="counter">
+    <button wire:click="increment">Add +1</button>
+    <div>{{ $count }}</div>
+
+    <style>
+        .counter {
+            display: flex;
+            align-items: center;
+            margin-block: 2rem;
+
+            div {
+                margin-left: 10px;
+                font-size: 1rem;
+                font-weight: bold;
+            }
+            button {
+                padding: 0.5rem 1rem;
+                font-size: 1rem;
+                background-color: #007bff;
+                color: white;
+                border: none;
+                border-radius: 0.5rem;
+                cursor: pointer;
+            }
+            button:hover {
+                background-color: #0056b3;
+            }
+            button:active {
+                background-color: #004085;
+            }
+        }
+    </style>
+</div>
+
+
